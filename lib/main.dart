e8cc4dc4fd8a2f27004cbd70718514bccffa615f
tiatiahwang <tiahwang44@gmail.com>
2023-03-29 21:49:23 -0700

    auto format setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,24 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xFF181818),
+        backgroundColor: const Color(0xFF181818),
         body: Padding(
-            padding: EdgeInsets.symmetric(
+            padding: const EdgeInsets.symmetric(
               horizontal: 40,
             ),
             child:
                 Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              SizedBox(
+              const SizedBox(
                 height: 80,
               ),
               Row(
@@ -26,7 +27,7 @@ class App extends StatelessWidget {
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      Text('Hey, Selena',
+                      const Text('Hey, Selena',
                           style: TextStyle(
                             color: Colors.white,
                             fontSize: 28,
@@ -41,7 +42,7 @@ class App extends StatelessWidget {
                   )
                 ],
               ),
-              SizedBox(
+              const SizedBox(
                 height: 120,
               ),
               Text('Total Balance',
@@ -49,16 +50,16 @@ class App extends StatelessWidget {
                     fontSize: 22,
                     color: Colors.white.withOpacity(0.8),
                   )),
-              SizedBox(
+              const SizedBox(
                 height: 5,
               ),
-              Text('\$5 194 482',
+              const Text('\$5 194 482',
                   style: TextStyle(
                     fontSize: 44,
                     fontWeight: FontWeight.w600,
                     color: Colors.white,
                   )),
-              SizedBox(
+              const SizedBox(
                 height: 30,
               ),
               Row(
@@ -69,7 +70,7 @@ class App extends StatelessWidget {
                         color: Colors.amber,
                         borderRadius: BorderRadius.circular(45),
                       ),
-                      child: Padding(
+                      child: const Padding(
                           padding: EdgeInsets.symmetric(
                             vertical: 15,
                             horizontal: 35,
@@ -80,10 +81,10 @@ class App extends StatelessWidget {
                               )))),
                   Container(
                       decoration: BoxDecoration(
-                        color: Color(0xFF1F2123),
+                        color: const Color(0xFF1F2123),
                         borderRadius: BorderRadius.circular(45),
                       ),
-                      child: Padding(
+                      child: const Padding(
                           padding: EdgeInsets.symmetric(
                             vertical: 15,
                             horizontal: 35,
