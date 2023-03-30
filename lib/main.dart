e51a545a300af8b62c7fde298d4d8a54fd0ce03d
tiatiahwang <tiahwang44@gmail.com>
2023-03-29 21:39:04 -0700

    UI - buttons

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,11 @@ class App extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Color(0xFF181818),
         body: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 40),
-            child: Column(children: [
+            padding: EdgeInsets.symmetric(
+              horizontal: 40,
+            ),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
               SizedBox(
                 height: 80,
               ),
@@ -36,6 +39,60 @@ class App extends StatelessWidget {
                           )),
                     ],
                   )
+                ],
+              ),
+              SizedBox(
+                height: 120,
+              ),
+              Text('Total Balance',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white.withOpacity(0.8),
+                  )),
+              SizedBox(
+                height: 5,
+              ),
+              Text('\$5 194 482',
+                  style: TextStyle(
+                    fontSize: 44,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  )),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(45),
+                      ),
+                      child: Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 15,
+                            horizontal: 35,
+                          ),
+                          child: Text('Transfer',
+                              style: TextStyle(
+                                fontSize: 20,
+                              )))),
+                  Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xFF1F2123),
+                        borderRadius: BorderRadius.circular(45),
+                      ),
+                      child: Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 15,
+                            horizontal: 35,
+                          ),
+                          child: Text('Request',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ))))
                 ],
               )
             ])),
