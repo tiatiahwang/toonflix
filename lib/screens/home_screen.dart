105453b789b12db094ca87ef9b1a3d053d0def68
tiatiahwang <tiahwang44@gmail.com>
2023-04-04 22:03:42 -0700

    pomodoro app - code challenge

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -31,6 +31,16 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  void onRestartPressed() {
+    if (totalSeconds == twentyFiveMinutes) return;
+    print('he');
+    setState(() {
+      isRunning = false;
+      totalSeconds = twentyFiveMinutes;
+    });
+    timer.cancel();
+  }
+
   void onStartPressed() {
     timer = Timer.periodic(
       const Duration(seconds: 1),
@@ -73,16 +83,26 @@ class _HomeScreenState extends State<HomeScreen> {
                 )),
           ),
           Flexible(
-            flex: 3,
-            child: Center(
-                child: IconButton(
-              onPressed: isRunning ? onPausePressed : onStartPressed,
-              icon: Icon(isRunning
-                  ? Icons.pause_circle_outline
-                  : Icons.play_circle_outlined),
-              iconSize: 120,
-              color: Theme.of(context).cardColor,
-            )),
+            flex: 2,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: isRunning ? onPausePressed : onStartPressed,
+                  icon: Icon(isRunning
+                      ? Icons.pause_circle_outline
+                      : Icons.play_circle_outlined),
+                  iconSize: 120,
+                  color: Theme.of(context).cardColor,
+                ),
+                IconButton(
+                  onPressed: onRestartPressed,
+                  icon: const Icon(Icons.restart_alt_rounded),
+                  iconSize: 40,
+                  color: Theme.of(context).cardColor,
+                )
+              ],
+            ),
           ),
           Flexible(
             flex: 1,
